0e97bd086996f5ea8f514e2e113dba9cccb6e680
Jan B. Krejčí <janbkrejci@gmail.com>
2024-09-25 16:45:31 +0000

    console log Commited from within lively.next.

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ export * from './ui/my-rectangle.cp.js';
 import { MyRectangle } from './ui/my-rectangle.cp.js';
 
 export async function main () {
-  new MyRectangle().openInWorld();
+  // new MyRectangle().openInWorld();
+  console.log('MyRectangle', MyRectangle);
 }
