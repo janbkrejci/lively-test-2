66ba9f718846dcc4d7409b0ccbd38163975599fa
Jan B. Krejčí <janbkrejci@gmail.com>
2024-09-25 07:21:59 +0000

    update Commited from within lively.next.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 'format esm';
+import {AMorph} from 'ui/a-morph.cp.js'
 export async function main () {
     // THIS FUNCTION IS THE ENTRY POINT IN THE BUNDLED APPLICATION!
+  new AMorph().openInWorld();
 }
