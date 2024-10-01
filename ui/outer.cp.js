60c5351701c79454af6b627c22709eae5177aae8
Jan B. Krejčí <janbkrejci@gmail.com>
2024-10-01 13:11:10 +0000

    update / relayout Commited from within lively.next.

--- ui/outer.cp.js
+++ ui/outer.cp.js
@@ -1,34 +1,37 @@
 import { ConstraintLayout, ShadowObject, component } from 'lively.morphic/index.js';
 import { pt } from 'lively.graphics/geometry-2d.js';
 import { Color } from 'lively.graphics/color.js';
+import { connect } from 'lively.bindings';
+
 'format esm';
 const Outer = component({
   name: undefined,
   dropShadow: new ShadowObject({ color: Color.black, blur: 15 }),
   clipMode: 'scroll',
   borderColor: Color.rgb(23, 160, 251),
-  extent: pt(1024, 768),
+  extent: $world.extent,
   fill: Color.rgb(194, 236, 201),
   layout: new ConstraintLayout({
-    lastExtent: {
-      x: 0,
-      y: 0
-    },
+    lastExtent: $world.extent,
     reactToSubmorphAnimations: false,
     submorphSettings: [['inner', {
       x: 'center',
       y: 'center'
     }]]
   }),
-  position: pt(297, 445.8),
+  position: pt(0, 0),
   submorphs: [{
     tooltip: 'Inner Morph',
     name: 'inner',
     borderColor: Color.rgb(23, 160, 251),
     extent: pt(320, 200),
-    fill: Color.rgb(243, 183, 183),
-    position: pt(512 - 160, 384 - 100)
-  }]
+    fill: Color.rgb(243, 183, 183)
+  }],
+  relayout: () => {
+    this.extent = $world.extent;
+  }
 });
+
+connect($world, 'extent', Outer, 'relayout');
 
 export { Outer };
